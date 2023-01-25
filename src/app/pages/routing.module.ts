e7059f180b6001bd9062fbc93a999a4ae29b739e
vituhonorato <vituhonorato.89@gmail.com>
2023-01-25 09:41:08 -0300

    add details page, svg pokemon and create routing module

--- src/app/pages/routing.module.ts
+++ src/app/pages/routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
 
-  {path:'',loadChildren: () => import('./pages/pages.module').then(p => p.PagesModule)}
+  {path:'',component: HomeComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class RoutingModule { }
