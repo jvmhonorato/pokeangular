7755953e33fb8d91d6cc19768d0c9243ca63ee01
vituhonorato <vituhonorato.89@gmail.com>
2023-01-25 09:58:03 -0300

    add module shared and poke-header component

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PokeHeaderComponent } from './poke-header/poke-header.component';
+
+
+
+@NgModule({
+  declarations: [
+    PokeHeaderComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
