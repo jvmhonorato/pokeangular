b004a88df1d1f8ea726ad772ed1e4e00b775923d
vituhonorato <vituhonorato.89@gmail.com>
2023-01-25 10:47:48 -0300

    create poke-search and declare in import and export at SharedModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PokeHeaderComponent } from './poke-header/poke-header.component';
+import { PokeSearchComponent } from './poke-search/poke-search.component';
 
 
 
 @NgModule({
   declarations: [
-    PokeHeaderComponent
+    PokeHeaderComponent,
+    PokeSearchComponent
   ],
   exports:[
-    PokeHeaderComponent
+    PokeHeaderComponent,
+    PokeSearchComponent
   ],
   imports: [
     CommonModule
