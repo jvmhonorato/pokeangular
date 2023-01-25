50ac32f3077a4e28241887ca1f237ca818dab05a
vituhonorato <vituhonorato.89@gmail.com>
2023-01-25 10:57:36 -0300

    create poke-list and declare in import and export at SharedModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PokeHeaderComponent } from './poke-header/poke-header.component';
 import { PokeSearchComponent } from './poke-search/poke-search.component';
+import { PokeListComponent } from './poke-list/poke-list.component';
 
 
 
 @NgModule({
   declarations: [
     PokeHeaderComponent,
-    PokeSearchComponent
+    PokeSearchComponent,
+    PokeListComponent
   ],
   exports:[
     PokeHeaderComponent,
-    PokeSearchComponent
+    PokeSearchComponent,
+    PokeListComponent
   ],
   imports: [
     CommonModule
